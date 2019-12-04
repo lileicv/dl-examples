70034bbdb51c3bbea9025ba2a4a550a19b3fcca7
lileicv <1466589627@qq.com>
2019-12-04 17:40:46 +0800

    update center_loss_pth

--- center_loss_pth/main.py
+++ center_loss_pth/main.py
@@ -2,17 +2,18 @@
 基于 pytorch 在 mnist 数据集上测试 center loss
 '''
 import numpy as np
-from keras.datasets import mnist
 from tqdm import tqdm
 import matplotlib as mlp
 mlp.use('Agg')
 import matplotlib.pyplot as plt
+import argparse
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import TensorDataset, DataLoader
+from torchvision import datasets, transforms
 
 class MainNet(nn.Module):
     def __init__(self):
@@ -24,7 +25,7 @@ class MainNet(nn.Module):
         self.conv5 = nn.Conv2d(64, 128, 3, padding=1)   # ==> 7,7,128
         self.conv6 = nn.Conv2d(128, 128, 3, padding=1)  # ==> 7,7,128
         self.fc1 = nn.Linear(3*3*128, 2)
-        self.fc2 = nn.Linear(2, 10)
+        self.fc2 = nn.Linear(2, 10, False)
     
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -49,25 +50,45 @@ class CenterLoss(nn.Module):
         center_loss = torch.pow(feature-center, 2).sum(1).mean()
         return center_loss
 
+class ProtoNet(nn.Module):
+    def __init__(self):
+        super(ProtoNet, self).__init__()
+        self.embedding = nn.Embedding(10, 2)
+        # self.centers = torch.nn.Parameter(torch.randn(10, 2)*1)
+
+    def forward(self, x):
+        x = torch.unsqueeze(x, 1)
+        centers = torch.unsqueeze(self.embedding.weight, 0)
+        x = -1*torch.pow(x-centers, 2).sum(-1)
+        y = F.log_softmax(x, 1)
+        return y, x
+
 if __name__=='__main__':
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--method', type=str, default='center_loss')
+    parser.add_argument('--epochs', type=int, default=10)
+    args = parser.parse_args()
+
     # 全局参数
-    epochs = 10
+    epochs = args.epochs
+    method = args.method # 'center_loss' or 'proto_loss'
+    assert(method in ['center_loss', 'proto_loss'])
+    print('method: {}'.format(method))
 
     # 加载数据
-    (xtr, ytr), (xte, yte) = mnist.load_data()
-    xtr = np.expand_dims(xtr, 1).astype('float32')/255.
-    xte = np.expand_dims(xte, 1).astype('float32')/255.
-    trset = TensorDataset(torch.from_numpy(xtr), torch.from_numpy(ytr).long())
-    teset = TensorDataset(torch.from_numpy(xte), torch.from_numpy(yte).long())
+    trset = datasets.MNIST(root='/home/lilei/.pytorch/datasets', train=True, transform=transforms.ToTensor())
+    teset = datasets.MNIST(root='/home/lilei/.pytorch/datasets', train=False, transform=transforms.ToTensor())
     trloader = DataLoader(trset, batch_size=128, shuffle=True, num_workers=8)
     teloader = DataLoader(teset, batch_size=128, num_workers=8)
 
     # 加载模型
     main_net = MainNet().cuda()
     cent_net = CenterLoss().cuda()
+    proto_net = ProtoNet().cuda()
     optim1 = optim.Adam(main_net.parameters(), lr=0.001)
-    optim2 = optim.Adam(cent_net.parameters(), lr=0.01)
+    optim2 = optim.Adam(cent_net.parameters(), lr=0.001)
+    optim3 = optim.Adam(proto_net.parameters(), lr=0.001)
 
     for epoch in range(epochs):
         main_net.train()
@@ -81,14 +102,27 @@ if __name__=='__main__':
         for i, (batchx, batchy) in enumerate(pbar):
             batchx, batchy = batchx.cuda(), batchy.cuda()
             logit, batchf = main_net(batchx)
-            xent_loss = F.nll_loss(logit, batchy)
-            cent_loss = cent_net(batchf, batchy)
-            loss = xent_loss + 0.2*cent_loss
+            if method=='center_loss':
+                xent_loss = F.nll_loss(logit, batchy)
+                cent_loss = cent_net(batchf, batchy)
+                loss = xent_loss + 0.2*cent_loss
+            elif method=='proto_loss':
+                logit, bbbb = proto_net(batchf)
+                # if epoch==5:
+                #     print(bbbb)
+                #     print(logit)
+                #     1/0
+                xent_loss = F.nll_loss(logit, batchy)
+                cent_loss = torch.tensor(0)
+                loss = xent_loss
+            
             optim1.zero_grad()
             optim2.zero_grad()
+            optim3.zero_grad()
             loss.backward()
             optim1.step()
             optim2.step()
+            optim3.step()
             accuracy = torch.eq(logit.argmax(1), batchy).float().mean().item()
 
             xent_losses.append(xent_loss.item())
@@ -99,15 +133,24 @@ if __name__=='__main__':
         # 预测
         main_net.eval()
         ete = []
+        yte = []
         for batchx, batchy in teloader:
             _, batchf = main_net(batchx.cuda())
             ete.append(batchf.cpu().detach().numpy())
+            yte.append(batchy.cpu().numpy())
         ete = np.concatenate(ete, 0)
+        yte = np.concatenate(yte, 0)
 
         # 画图
+        if method=='center_loss':
+            centers = cent_net.embedding.weight.cpu().detach().numpy()
+        elif method=='proto_loss':
+            centers = proto_net.embedding.weight.cpu().detach().numpy()
+
         for j in range(10):
             x = ete[yte==j, 0]
             y = ete[yte==j, 1]
             plt.scatter(x, y, label='{}'.format(j), marker='.', alpha=0.1)
-        plt.savefig('./figures/center_loss_{}.png'.format(epoch))
+            plt.scatter(centers[j,0], centers[j,1], marker='.', c='b')
+        plt.savefig('./figures/{}_{}.png'.format(method, epoch))
         plt.close()
